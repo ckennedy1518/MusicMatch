79e5d72737c0949073f696793879c2b9024152ca
XanGrab <sunbird.grabowski@gmail.com>
2022-10-16 07:54:49 -0500

    + styles * next package

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,64 @@
-import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 import ourLogo from "./assets/couplet-icon.svg";
-import "./App.css";
+import "./styles/App.css";
 import { loginUrl } from "./spotify";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // fetch user token for spotify
+  const [token, setToken] = useState("");
+  // fetch spotify data
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  /**
+   * @param e - DOM element
+   */
+  const searchArtists = async (e: any) => {
+    e.preventDefault();
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: searchKey,
+        type: "artist",
+      },
+    });
+
+    setArtists(data.artists.items);
+  };
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token")!;
+
+    if (token && hash) {
+      token = hash
+        ?.substring(1)
+        ?.split("&")
+        ?.find((elem) => elem.startsWith("access_token"))
+        ?.split("=")[1]!;
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    if (token != null) {
+      setToken(token);
+    } else {
+      console.log("[Error] token is null.");
+    }
+
+    // EXTRA
+    searchArtists;
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
 
   return (
     <div className="App">
